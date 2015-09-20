4ff8dd8b30b47d2c0ae95c2ac7e39a683d63bc70
Xiaokai Sun <xiaokais@andrew.cmu.edu>
2015-09-20 18:27:22 -0400

    first commit

---
+++ src/QrySopAdd.java
@@ -0,0 +1,51 @@
+/**
+ *  Copyright (c) 2015, Carnegie Mellon University.  All Rights Reserved.
+ */
+
+import java.io.*;
+
+/**
+ *  The OR operator for all retrieval models.
+ */
+public class QrySopAdd extends QrySop {
+
+  /**
+   *  Indicates whether the query has a match.
+   *  @param r The retrieval model that determines what is a match
+   *  @return True if the query matches, otherwise false.
+   */
+  public boolean docIteratorHasMatch (RetrievalModel r) {
+    return this.docIteratorHasMatchUnrankedAdd (r);
+  }
+
+  /**
+   *  Get a score for the document that docIteratorHasMatch matched.
+   *  @param r The retrieval model that determines how scores are calculated.
+   *  @return The document score.
+   *  @throws IOException Error accessing the Lucene index
+   */
+  public double getScore (RetrievalModel r) throws IOException {
+
+    if (r instanceof RetrievalModelUnrankedBoolean) {
+      return this.getScoreUnrankedBoolean (r);
+    } else {
+      throw new IllegalArgumentException
+        (r.getClass().getName() + " doesn't support the ADD operator.");
+    }
+  }
+  
+  /**
+   *  getScore for the UnrankedBoolean retrieval model.
+   *  @param r The retrieval model that determines how scores are calculated.
+   *  @return The document score.
+   *  @throws IOException Error accessing the Lucene index
+   */
+  private double getScoreUnrankedBoolean (RetrievalModel r) throws IOException {
+    if (! this.docIteratorHasMatchCache()) {
+      return 0.0;
+    } else {
+      return 1.0;
+    }
+  }
+
+}
